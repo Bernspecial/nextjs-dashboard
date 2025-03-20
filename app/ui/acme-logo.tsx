64b4e649271ad34a9abf9e438f2ceab29becc10f
Bernspecial <michaelbernardolayemi@gmail.com>
2025-03-20 19:19:34 +0100

    Acme correction

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,20 +1,13 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
-import { Suspense } from 'react';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from "@/app/ui/fonts";
 
-export default function LoginPage() {
+export default function AcmeLogo() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
-          </div>
-        </div>
-        <Suspense>
-          <LoginForm />
-        </Suspense>
-      </div>
-    </main>
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p>
+    </div>
   );
 }
